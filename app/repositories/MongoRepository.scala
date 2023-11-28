16513d710a49af2c9c0a75ee78875c578e5eab37
Derick Damoah Twum <derickdamoahtwum@Dericks-Mac-mini.local>
2023-11-28 23:09:50 +0000

    wip

--- app/repositories/MongoRepository.scala
+++ app/repositories/MongoRepository.scala
@@ -10,6 +10,7 @@ class MongoRepository {
   val mongodbconfig = ConfigFactory.load().getConfig("mongodb")
   private val dotenv = Dotenv.configure().ignoreIfMissing().load()
   private val connection_string: String = Try(mongodbconfig.getString("connectionString")).getOrElse(s"mongodb+srv://${dotenv.get("MONGO_DB_USERNAME")}:${dotenv.get("MONGO_DB_PASSWORD")}@todo-list-cluster.elkwww6.mongodb.net/?retryWrites=true&w=majority")
+  println(Console.GREEN + connection_string + Console.RESET)
   private val database_name: String = mongodbconfig.getString("database")
   private val collection_name: String = mongodbconfig.getString("collection")
   lazy val mongoClient: MongoClient = MongoClient(connection_string)
